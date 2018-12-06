1dee75caa0572b8bc425bdd432ca4eb44a3865c6
Sandra Dylus <ichistmeinname@web.de>
2018-12-06 21:40:10 +0100

    Create second exercise as python file

---
+++ Exercises/Sheet1/exercise2.py
@@ -0,0 +1,14 @@
+# In der Vorlesung haben wir Operatoren in Ausdrücken und ihre Präzedenzen kennen gelernt. So binden in Python die Operatoren 
+#   `*` und `**` stärker als `+` und `-` (vgl. "Punkt-vor-Strich"-Rechnung).
+
+# (a) Untersuchen Sie, die Präzedenzen von `**`, `*` und `/` untereinander. Finden Sie also heraus, ob ein Operator stärker 
+#   bindet bzw. welche Operatoren gleich stark binden. Argumentieren Sie mit Hilfe von Beispielausdrücken (geklammert und 
+#   ungeklammert) und ihrer Auswertung in Python. (Tipp: Für den vergleich der Bindungsstärke von `*` und `/` kann es hilfreich 
+#   sein, die Rundungsfehler für Float-Zahlen zu nutzen).
+
+# (b) Bei der Verwendung von Operatoren, die gleich stark binden, ist es auch noch wichtig zu wissen, ob sie von links nach 
+#   rechts oder von rechts nach links binden. Überlegen Sie, bei welchen Operatoren dies einen Unterschied macht und geben Sie 
+#   Beispiele an, an denen man das Bindungsverhalten erkennen kann.
+
+# (c) Auch die booleschen Operatoren **AND** und **OR** haben unterschiedliche Bindungsstärken. Untersuchen Sie nach dem 
+#   gleichen Prinzip auch diese Operatoren.
