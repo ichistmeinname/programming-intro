e77a2840ad0a8016bf6313d9334da274c39354d3
Sandra Dylus <ichistmeinname@web.de>
2018-12-06 21:37:30 +0100

    Create first exercise as python file

---
+++ Exercises/Sheet1/exercise1.py
@@ -0,0 +1,7 @@
+# Um ein wenig mit Python warm zu werden, formulieren wir ein paar Beispielausdrücke mit festgelegten Werten.
+
+# * Wiederholen Sie die Operatoren **AND** und **OR**, indem Sie für beide einen Python-Ausdruck mit `... if ... else ...` angeben.
+# * Geben Sie einen Python-Ausdruck an, der den Betragswert einer Zahl berechnet, also z. B. |3| = 3, |0| = 0 und |-42| = 42.
+# * Diskutieren Sie über weitere beispielhafte Python-Ausdrücke, die Sie implementieren können.
+
+# Realisieren Sie die Ausdrücke für **OR** und den Betragswert auch in einer Tabellenkalkulation Ihrer Wahl.
